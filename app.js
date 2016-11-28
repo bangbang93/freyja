05a720ebbf1afb060d85f386f879cff64ee37bfe
bangbang93 <bangbang93@163.com>
2016-11-28 16:03:22 +0800

    init

---
+++ app.js
@@ -0,0 +1,48 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+if (app.get('env') == 'development'){
+  app.use(function (err, req, res, next) {
+    res.status(500).json({
+      message: err.message,
+      err,
+    })
+  })
+} else {
+  app.use(function(err, req, res, next) {
+    res.status(500).json({
+      message: err.message,
+    })
+  });
+}
+
+module.exports = app;
