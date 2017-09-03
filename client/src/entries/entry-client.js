03306b33ed2cbe0a4b34b279fdcec7d594a65864
bangbang93 <bangbang93@163.com>
2017-09-03 15:19:23 +0800

    ssr ready

--- client/src/entries/entry-client.js
+++ client/src/entries/entry-client.js
@@ -7,6 +7,10 @@ import {createApp} from './index'
 
 const {app, router, store} = createApp()
 
+if (window.__INITIAL_STATE__) {
+  store.replaceState(window.__INITIAL_STATE__)
+}
+
 router.onReady(() => {
   router.beforeResolve((to, from, next) => {
     const matched = router.getMatchedComponents(to)
