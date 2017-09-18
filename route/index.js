32395c72a1dc1d71662c5a570145b656eb202567
bangbang93 <bangbang93@163.com>
2017-09-18 18:53:06 +0800

    兼容路由使用301

--- route/index.js
+++ route/index.js
@@ -12,7 +12,7 @@ router.get('/:year(\\d+)/:mouth(\\d+)/:day(\\d+)/:postName.:suffix', async funct
   const {postName} = req.params
   const article = await ArticleService.getByWordpress({postName})
   if (!article) return next()
-  res.redirect(`/article/${article._id}`)
+  res.redirect(301, `/article/${article._id}`)
 })
 
 router.get('/', async function (req, res, next) {
@@ -20,7 +20,7 @@ router.get('/', async function (req, res, next) {
   if (!p) return next()
   const article = await ArticleService.getByWordpress({id: p})
   if (!article) return next()
-  res.redirect(`/article/${article._id}`)
+  res.redirect(301, `/article/${article._id}`)
 })
 
 module.exports = router;
