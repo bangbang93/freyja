ffaa8c2f3be4692560e136e8cf7eab1f724ed735
bangbang93 <bangbang93@163.com>
2023-03-07 09:17:31 +0800

    fix: remove console

--- src/app/admin/admin.guard.ts
+++ src/app/admin/admin.guard.ts
@@ -5,7 +5,6 @@ import {Request} from 'express'
 export class AdminGuard implements CanActivate {
   public canActivate(context: ExecutionContext): boolean {
     const req = context.switchToHttp().getRequest<Request>()
-    console.log(req.session)
     return !!req.session?.user
   }
 }
