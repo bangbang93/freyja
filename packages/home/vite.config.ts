aaea22831097d69b42dfa7db8b411247f592b0e5
bangbang93 <bangbang93@163.com>
2025-03-23 10:32:21 +0800

    feat: add vite-plugin-prismjs

--- packages/home/vite.config.ts
+++ packages/home/vite.config.ts
@@ -1,11 +1,14 @@
 import vuePlugin from '@vitejs/plugin-vue'
 import vike from 'vike/plugin'
 import {defineConfig} from 'vite'
+import prismjs from 'vite-plugin-prismjs'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vuePlugin(),
     vike(),
+    prismjs(),
   ],
 })
